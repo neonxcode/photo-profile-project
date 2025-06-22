0a6ec9e9795f98f1a2c8af8e4ff2cefb826da0ad
neonxcode <neonxcodez@gmail.com>
2025-06-22 17:22:10 +0000

    added aboutme

--- src/components/images-grid/images-grid.tsx
+++ src/components/images-grid/images-grid.tsx
@@ -3,7 +3,7 @@
 import { MasonryGallery } from "@/components/MasonryGallery";
 
 const images = [
-  "/images/1465556313372.jpg",
+  "/images/15.png",
   "https://images.unsplash.com/photo-1505144808419-1957a94ca61e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3070&q=80",
   "/images/img1.jpg",
   "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3387&q=80",
